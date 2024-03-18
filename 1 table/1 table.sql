58d22e45e5b50cebc663309944eb87827467bf32
chanamindy <chanamindyh@gmail.com>
2024-03-18 11:35:39 -0400

    1st commit for the trial branch

--- 1 table/1 table.sql
+++ 1 table/1 table.sql
@@ -1,5 +1,9 @@
 use humanbodyDB
-create table dbo.body(id int not null identity primary key, 
+go 
+drop table if exists body
+create table dbo.body(
+id int not null identity primary key, 
+creaturetype varchar(25) not null,
 bodypart varchar(100) not null, 
 num int not null
 )
